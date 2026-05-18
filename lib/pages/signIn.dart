5492d718cd9eef65df0e1c9a11309a16659d17d3
HaneenT7 <hneen2233@gmail.com>
2026-05-18 21:49:39 +0300

    conflict solved

--- lib/pages/signIn.dart
+++ lib/pages/signIn.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'Dashboard.dart';
+import 'DashBoard.dart' show DashBoard;
 import 'signUp.dart';
 import 'reset_password.dart';
 
